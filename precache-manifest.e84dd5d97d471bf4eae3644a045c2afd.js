cc9b1ce2e60832f8c45120b0bacf7dfeaf4928e0
dusanpopov <aristocut90@gmail.com>
2020-02-05 18:39:02 +0100

    Updates

---
+++ precache-manifest.e84dd5d97d471bf4eae3644a045c2afd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a2bf00564acda9bd3aa651351a0ebae",
+    "url": "/React-Hooks-Weather-App/index.html"
+  },
+  {
+    "revision": "093a8aab4c49cc8acf6a",
+    "url": "/React-Hooks-Weather-App/static/css/main.2a56777c.chunk.css"
+  },
+  {
+    "revision": "f0d4c1c8fc9664207699",
+    "url": "/React-Hooks-Weather-App/static/js/2.1f0b71c5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React-Hooks-Weather-App/static/js/2.1f0b71c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "093a8aab4c49cc8acf6a",
+    "url": "/React-Hooks-Weather-App/static/js/main.af0d260f.chunk.js"
+  },
+  {
+    "revision": "146264528f2a0ca72680",
+    "url": "/React-Hooks-Weather-App/static/js/runtime-main.b0f97e29.js"
+  }
+]);
