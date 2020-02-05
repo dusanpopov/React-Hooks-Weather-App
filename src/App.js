a7f311db54968fd38c98a7276d219162ec8fd945
dusanpopov <aristocut90@gmail.com>
2020-02-05 18:37:55 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import {API_DATA} from "./components/Apikey";
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const search = (e) => {
+
+    if(e.key === "Enter"){
+      fetch(`${API_DATA.base}weather?q=${query}&units=metric&APPID=${API_DATA.key}`)
+      .then(res => res.json())
+      .then(result => {
+
+        setWeather(result)
+        setQuery("")})
+
+    }
+
+  }
+  
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+      <div className="search-box">
+        <input type="text" className="search-bar" placeholder="Search" value={query} 
+        onChange={e => setQuery(e.target.value)} 
+        onKeyPress={search}/>
+      </div>
+
+      {(typeof weather.main != "undefined") ? (
+
+      <div class="container-info">
+
+          <div className="location-box">
+            <h2 className="location">{weather.name}, {weather.sys.country}</h2>
+          </div>
+
+          <div className="weather-box">
+            <p className="temp">
+              {Math.round(weather.main.temp)}°c
+            </p>
+            <p className="weather">{weather.weather[0].main}</p>
+          </div>
+
+        </div>
+
+        ) : ('')}
+
+      </div>
+
+    )
 }
 
 export default App;
---
+++ src/components/Apikey.js
@@ -0,0 +1,5 @@
+export const API_DATA = {
+    key: "c5ff356c290112c05c15bb06c00cc0fa",
+    base: "https://api.openweathermap.org/data/2.5/"
+  }
+
